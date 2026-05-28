53e10e590a8b7e634e0192cc91c29dcae73905a5
empxiaoking <empxiaoking@gmail.com>
2026-05-28 21:18:32 +0800

    chore: 删除模板残留文件（blog/auth/prerender/sdk），精简依赖

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,10 +2,7 @@ import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
-import BlogRoutes from './blog-routes';
 import Index from './pages/Index';
-import AuthCallback from './pages/AuthCallback';
-import AuthError from './pages/AuthError';
 
 // GitHub Pages 部署使用 BrowserRouter（HTTPS 同源，无 CORS 问题）
 // 本地 file:// 双击打开时回退到 HashRouter
@@ -22,9 +19,6 @@ const queryClient = new QueryClient();
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Index />} />
-    {/* <Route path="/blog/*" element={<BlogRoutes />} /> */}
-    <Route path="/auth/callback" element={<AuthCallback />} />
-    <Route path="/auth/error" element={<AuthError />} />
     {/* MODULE_ROUTES_START */}
     {/* MODULE_ROUTES_END */}
   </Routes>
